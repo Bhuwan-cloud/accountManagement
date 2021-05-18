b2b94d11af9caf438186640913ebd2f300d3f248
Bhuwan-cloud <bhuwanbhatt475@gmail.com>
2021-05-18 12:58:18 +0545

    rails admin removed from routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :teams
   resources :homepages
   devise_for :users
-  mount RailsAdmin::Engine => '/admins', as: 'rails_admin'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'homes#index'
